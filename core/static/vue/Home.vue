bfa7fdaf54f217e91cdcf2358e4128efbef114d0
Bennani <boredomdidit+github@gmail.com>
2025-07-17 18:55:17 +0200

    d

---
+++ core/static/vue/Home.vue
@@ -0,0 +1,25 @@
+<script setup>
+import BestSellingWidget from '@BestSellingWidget.vue';
+import NotificationsWidget from '@4.3.6NotificationsWidget.vue';
+import RecentSalesWidget from '@4.3.6RecentSalesWidget.vue';
+import RevenueStreamWidget from '@4.3.6RevenueStreamWidget.vue';
+import StatsWidget from '@4.3.6StatsWidget.vue';
+
+import currentScope from './Base.vue';
+currentScope.scope = 'home';
+</script>
+
+<template>
+    <div class="grid grid-cols-12 gap-8">
+        <StatsWidget />
+
+        <div class="col-span-12 xl:col-span-6">
+            <RecentSalesWidget />
+            <BestSellingWidget />
+        </div>
+        <div class="col-span-12 xl:col-span-6">
+            <RevenueStreamWidget />
+            <NotificationsWidget />
+        </div>
+    </div>
+</template>
---
+++ static/vue/Base.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {reactive} from 'vue'
+
+export const currentScope = reactive({
+  scope: null,
+})
+
+</script>
+<template>
+    <component :is="currentScope"/>
+</template>
+
+<style scoped></style>
